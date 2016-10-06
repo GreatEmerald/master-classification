ea7f356d24adddc573b466716ccde221a9bd08fa
Dainius <pastas4@gmail.com>
2016-10-06 14:31:31 +0200

    Addition of CCI simplification script

---
+++ src/simplify-cci.r
@@ -0,0 +1,44 @@
+library(raster)
+library(rgdal)
+# Merging CCI types:
+# Crops: 10 (Cropland rainfed), 11 (Cropland rainfed - Herbaceous cover), 12 (Cropland rainfed - Tree or shrub cover),
+#        13 (Cropland irrigated or post-flooding), 20 (Cropland irrigated or post-flooding),
+#        30 (Mosaic cropland (>50%) / natural vegetation (tree/shrub/herbaceous cover) (<50%))
+# Deciduous trees: 60 (Tree cover  broadleaved  deciduous  closed to open (>15%)),
+#                  61 (Tree cover  broadleaved  deciduous  closed (>40%)),
+#                  62 (Tree cover  broadleaved  deciduous  open (15-40%)),
+#                  80 (Tree cover  needleleaved  deciduous  closed to open (>15%)),
+#                  81 (Tree cover  needleleaved  deciduous  closed (>40%)),
+#                  82 (Tree cover  needleleaved  deciduous  open (15-40%)),
+#                  90 (Tree cover  mixed leaf type (broadleaved and needleleaved))
+# Evergreen trees: 50 (Tree cover broadleaved evergreen closed to open (>15%)),
+#                  70 (Tree cover  needleleaved  evergreen  closed to open (>15%)),
+#                  71 (Tree cover  needleleaved  evergreen  closed (>40%))
+#                  72 (Tree cover  needleleaved  evergreen  open (15-40%))
+# Shrublands: 100 (Mosaic tree and shrub (>50%) / herbaceous cover (<50%)), 120 (Shrubland), 121 (Shrubland evergreen),
+#             122 (Shrubland deciduous)
+# Grasslands: 130 (Grassland), 140 (Lichens and mosses), 110 (Mosaic herbaceous cover (>50%) / tree and shrub (<50%)),
+#             40 (Mosaic natural vegetation (tree/shrub/herbaceous cover) (>50%) / cropland (<50%))
+# Bare soil: 150 (Sparse vegetation (tree/shrub/herbaceous cover) (<15%)), 152 (Sparse shrub (<15%)),
+#            153 (Sparse herbaceous cover (<15%)), 200 (Bare areas), 201 (Consolidated bare areas),
+#            202 (Unconsolidated bare areas)
+# Wetlands: 160 (Tree cover flooded fresh or brakish water), 170 (Tree cover flooded saline water),
+#           180 (Shrub or herbaceous cover flooded fresh/saline/brakish water)
+# Urban: 190 (Urban areas)
+# Water: 210 (Water bodies)
+# Permanent snow or ice: 220 (Permanent snow and ice)
+
+CCI = raster("/home/dainius/Downloads/ESACCI-LC-L4-LCCS-Map-300m-P5Y-2010-v1.6.1.tif")
+AOI = readOGR("/home/dainius/Downloads/probavextent/aoi.shp", "aoi")
+CCI.crop = crop(CCI, AOI)
+# range min, range max, overwritten by what
+reclassifier = c(
+    10, 30, 10,
+    60, 62, 60, 80, 90, 60,
+    70, 72, 70, 50, 50, 70,
+    120, 122, 120, 100, 100, 120,
+    130, 140, 130, 40, 40, 130, 110, 110, 130,
+    200, 202, 200, 150, 153, 200,
+    160, 180, 160)
+CCI.simple = reclassify(CCI.crop, reclassifier, right=NA)
+writeRaster(CCI.simple, "/home/dainius/Downloads/probavextent/cci-simplified.tif")
