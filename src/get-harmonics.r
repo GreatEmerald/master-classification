7f11f12b154f3ba79daf6f4ef41cdc8ee6230042
GreatEmerald <pastas4@gmail.com>
2017-02-06 11:50:00 +0000

    Fix for parameters when computing spatial harmonics

--- src/get-harmonics.r
+++ src/get-harmonics.r
@@ -18,19 +18,20 @@ ymin <- 58
 ymax <- 59
 
 BandPattern = "(BLUE|NDVI)_sm.tif$"
-VrtFilename = paste0(OutputDir, "harmonics.vrt")
+VrtFilename = paste0(OutputDir, "harmonics2.vrt")
 
 # Create virtual stack
 Vrt = timeVrtProbaV(SemiCleanDir, pattern = BandPattern, vrt_name = VrtFilename, tile = TileOfInterest,
     return_raster = TRUE, te = c(xmin, ymin, xmax, ymax))
 
 TS = timeVrtProbaV(SemiCleanDir, pattern = BandPattern, vrt_name = VrtFilename, tile = TileOfInterest,
-    return_raster = FALSE)
+    return_raster = FALSE, te = c(xmin, ymin, xmax, ymax))
 
 Bands = TS[TS$date == TS$date[1], 'band']
 Dates = TS[TS$band == Bands[1], 'date']
 
 Dates2 = TS[TS$band == Bands[2], 'date']
+Dates == Dates2
 
 RowsPerThread = 4
 Cores = 30
@@ -45,3 +46,36 @@ out_name = OutputFile
 Harmonics = system.time(getHarmMetricsSpatial(Vrt, TS, minrows = RowsPerThread, mc.cores = Cores,
     logfile=LogFile, overwrite=TRUE, span=0.3, cf_bands = 1, thresholds=c(-30, Inf),
     filename = OutputFile, order = 2, datatype="FLT4S"))
+
+phaser = function(co, si)
+{
+    tau = 2*pi
+    return(atan2(si, co) %% tau)
+}
+amplituder = function(co, si)
+{
+    return(sqrt(co^2 + si^2))
+}
+
+# Apply phaser and amplituder to our test data
+LakeCoeffs = brick(OutputFile)
+# min max intercept co si co2 si2 co3 si3 trend (blue, NDVI)
+# min max intercept co si co2 si2 trend (blue, NDVI)
+#spplot(LakeCoeffs[[20]])
+#p1 = phaser(LakeCoeffs[[12]], LakeCoeffs[[13]])
+p1 = phaser(LakeCoeffs[[4]], LakeCoeffs[[5]])
+#spplot(p1)
+#a1 = amplituder(LakeCoeffs[[12]], LakeCoeffs[[13]])
+a1 = amplituder(LakeCoeffs[[4]], LakeCoeffs[[5]])
+#spplot(a1)
+#p2 = phaser(LakeCoeffs[[14]], LakeCoeffs[[15]])
+p2 = phaser(LakeCoeffs[[6]], LakeCoeffs[[7]])
+#spplot(p2)
+#a2 = amplituder(LakeCoeffs[[14]], LakeCoeffs[[15]])
+a2 = amplituder(LakeCoeffs[[6]], LakeCoeffs[[7]])
+#spplot(a2)
+#p3 = phaser(LakeCoeffs[[18]], LakeCoeffs[[19]])
+#spplot(p3)
+#a3 = amplituder(LakeCoeffs[[18]], LakeCoeffs[[19]])
+#spplot(a3)
+brick(p1, a1, p2, a2, filename=paste0(OutputDir, "phase-amplitude.tif"), overwrite=TRUE)
