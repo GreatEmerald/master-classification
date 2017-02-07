02035312781f8f7a0163d9a182e0b248d5d361c4
GreatEmerald <pastas4@gmail.com>
2017-02-07 15:40:21 +0000

    Additional documentation on optical processing steps
    Also made cloud timeseries cleaning run on a whole tile again

--- src/clean-timeseries.r
+++ src/clean-timeseries.r
@@ -1,4 +1,8 @@
 # Clean images using the time series method
+#
+# Input: blue band images.
+# Output: a mask file (QCFile), and it applied to NDVI to get an NDVI time series (CleanNDVI)
+
 library(probaV)
 library(tools)
 source("utils/set-temp-path.r")
@@ -9,24 +13,25 @@ OutputDir = "../../userdata/composite/tscleaned/"
 TileOfInterest = "X20Y01"
 
 # Subset for testing
-xmin <- 27
-xmax <- 28
-ymin <- 58
-ymax <- 59
+#xmin <- 27
+#xmax <- 28
+#ymin <- 58
+#ymax <- 59
 
 # Create a virtual raster (faster stack) from all blue bands
 VrtPattern = "(BLUE)_sm.tif$"
 VrtName = file.path(paste0(OutputDir,TileOfInterest, "_",paste0(VrtPattern, collapse = "_"), ".vrt"))
+file.remove(VrtName)
 Vrt = timeVrtProbaV(IntermediaryDir, pattern = VrtPattern, vrt_name = VrtName, tile = TileOfInterest,
-    return_raster = TRUE,
-    te = c(xmin, ymin, xmax, ymax))
+    return_raster = TRUE)#,
+    #te = c(xmin, ymin, xmax, ymax))
 
 # QC file and log file names
-QCFile = paste0(OutputDir, 'ts-mask.tif')
+QCFile = paste0(OutputDir, 'ts-mask-whole.tif')
 LogFile = paste0(OutputDir, 'ts-cleaning.log')
 
-Cores = 31
-Rows = 3
+Cores = 16
+Rows = 12
 psnice(value = min(Cores - 1, 19))
 system.time(
     QCMask <- cloud_filter(x = Vrt, probav_sm_dir = IntermediaryDir, pattern = VrtPattern,
@@ -49,8 +54,8 @@ which(names(Vrt) == "PROBAV_S5_TOC_X20Y01_20160711_100M_V101_BLUE_sm.tif")
 NDVIDir = "../../userdata/semicleaned/ndvi"
 NDVIs = stack(list.files(NDVIDir, pattern=glob2rx("*NDVI*.tif"), full.names = TRUE))
 TSCleanDir = "../../userdata/composite/tscleaned/"
-QCMask = brick(paste0(TSCleanDir, QCFile))
+QCMask = brick(QCFile)
 CleanNDVI = mask(NDVIs, QCMask, maskvalue=c(2,0),
-    filename=paste0(TSCleanDir, "CleanNDVI.tif"), datatype="FLT4S", progress="text")#,
-    #options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "NUMTHREADS=30"))
+    filename=paste0(TSCleanDir, "CleanNDVI.tif"), datatype="FLT4S", progress="text",
+    overwrite=TRUE, options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "NUMTHREADS=30"))
 
