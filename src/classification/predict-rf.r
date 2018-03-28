3220971b73df0c86da34cb0b2e475b840ce18dc0
GreatEmerald <pastas4@gmail.com>
2018-03-28 14:44:06 +0000

    Addition of an example Jupyter notebook
    Also changes needed to demonstrate the UAR tests from within one

--- src/classification/predict-rf.r
+++ src/classification/predict-rf.r
@@ -6,12 +6,33 @@ library(raster)
 source("utils/set-temp-path.r")
 source("utils/load-data.r")
 
-Cores = 31
-TempDir = "../../userdata/temp/"
-OutputDir = "../../userdata/predictions/"
+if (!exists("Cores"))
+    Cores = 31
+if (!exists("TempDir"))
+    TempDir = "../../userdata/temp/"
+if (!exists("OutputDir"))
+    OutputDir = "../../userdata/predictions/"
 
-TrainingRasters = LoadTrainingRasters()
-TrainingNames = GetTrainingNames(exclude=c("osavi", "aspect", "is.water", "height"))
+if (!exists("Extent"))
+    Extent = NULL
+if (!exists("Exclude"))
+    Exclude = c("osavi", "aspect", "is.water", "height")
+if (exists("CovariateDir"))
+{
+    TrainingRasters = LoadTrainingRasters(extent=Extent, basedir=CovariateDir)
+} else {
+    TrainingRasters = LoadTrainingRasters(extent=Extent)
+}
+if (!exists("ClassesToPredict"))
+{
+    ClassesToPredict = GetValidationNames()
+}
+if (!exists("Alpha"))
+    Alpha = 0.9
+if (!exists("MinProp"))
+    MinProp = 0.11
+
+TrainingNames = GetTrainingNames(exclude=Exclude)
 alldata = LoadClassificationData()
 
 RP = function(...)
@@ -24,11 +45,19 @@ RP = function(...)
 FullFormula = paste0("~", paste(TrainingNames, collapse = "+"))
 AllPredictions = stack()
 print("Starting prediction phase 1...")
-for (Class in GetValidationNames())
+for (Class in ClassesToPredict)
 {
     print(Class)
     Formula = update.formula(FullFormula, paste0(Class, " ~ ."))
-    rfmodel = ranger(Formula, alldata@data, splitrule="maxstat", alpha=0.9, minprop=0.11, seed=0xbadcafe)
+    if (exists("GetVariableImportance") && GetVariableImportance)
+    {
+        rfmodel = ranger(Formula, alldata@data, splitrule="maxstat", alpha=Alpha, minprop=MinProp, importance="permutation", seed=0xbadcafe)
+        print(rfmodel$prediction.error)
+        print(importance(rfmodel))
+        barplot(importance(rfmodel), main=Class)
+    } else {
+        rfmodel = ranger(Formula, alldata@data, splitrule="maxstat", alpha=Alpha, minprop=MinProp, seed=0xbadcafe)
+    }
 
     print(system.time(predicted <- predict(TrainingRasters, rfmodel, fun=RP, num.threads=Cores,
         filename=paste0(TempDir, Class, ".grd"), progress="text", overwrite=TRUE, seed=0xbadcafe)))
@@ -36,14 +65,21 @@ for (Class in GetValidationNames())
     AllPredictions = stack(AllPredictions, predicted)
 }
 
-print("Starting prediction phase 2...")
-RasterScaling = function(x)
+# Only scale the predictions to 100% if we have all classes
+if (length(ClassesToPredict) >= 9)
 {
-    return(round(x / sum(x) * 100))
+    print("Starting prediction phase 2...")
+    RasterScaling = function(x)
+    {
+        return(round(x / sum(x) * 100))
+    }
+    system.time(ScaledPredictions <- calc(AllPredictions, RasterScaling, progress="text"))
+    names(ScaledPredictions) = GetValidationNames()
+    print("Writing...")
+    ScaledPredictions = writeRaster(ScaledPredictions, filename = paste0(OutputDir, "randomforest.tif"),
+        datatype="INT1U", options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "NUMTHREADS=4"), overwrite=TRUE)
+    hdr(ScaledPredictions, "VRT")
+} else {
+    writeRaster(AllPredictions, filename = paste0(OutputDir, "randomforest.tif"),
+        datatype="INT1U", options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "NUMTHREADS=4"), overwrite=TRUE)
 }
-system.time(ScaledPredictions <- calc(AllPredictions, RasterScaling, progress="text"))
-names(ScaledPredictions) = GetValidationNames()
-print("Writing...")
-ScaledPredictions = writeRaster(ScaledPredictions, filename = paste0(OutputDir, "randomforest.tif"),
-    datatype="INT1U", options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "NUMTHREADS=4"), overwrite=TRUE)
-hdr(ScaledPredictions, "VRT")
