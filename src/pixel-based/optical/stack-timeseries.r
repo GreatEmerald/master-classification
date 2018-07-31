9defba581213b6cc3ebc2384e9c51efc18da9d70
GreatEmerald <pastas4@gmail.com>
2018-07-31 14:04:10 +0200

    Finish the initial version of stack-timeseries
    It now runs autonomously on multiple cores to extract pixel data
    and ssaves them as CSV files (those will be included later)

--- src/pixel-based/optical/stack-timeseries.r
+++ src/pixel-based/optical/stack-timeseries.r
@@ -1,24 +1,23 @@
 # Generates VRTs for all the Proba-V imagery to use as a stack.
 library(gdalUtils)
 library(raster)
+library(foreach)
+library(doParallel)
 source("pixel-based/utils/getProbaVInfo.r")
 source("pixel-based/utils/load-sampling-data.r")
 source("utils/GetProbaVQCMask.r")
 source("pixel-based/utils/ProbaVDataDirs.r")
 
 DataDir = "/data/MTDA/TIFFDERIVED/PROBAV_L3_S5_TOC_100M"
-OutputDir = "../../userdata/master-classification/pixel-based/vrt/sr/"
+OutputDir = "../data/pixel-based"
 
+SamplePoints = LoadGlobalTrainingData()
+
+# Tiles to process
 TileList = expand.grid(sprintf("X%02d", 15:23), sprintf("Y%02d", 3:10), stringsAsFactors = FALSE)
 TileList = paste0(TileList[,1], TileList[,2])
-# There's just sea here
-ExcludeTiles = expand.grid(sprintf("X%02d", 15:18), sprintf("Y%02d", 8:10), stringsAsFactors = FALSE)
-ExcludeTiles = paste0(ExcludeTiles[,1], ExcludeTiles[,2])
-ExcludeTiles = c(ExcludeTiles, "X15Y07", "X16Y07", "X23Y07", "X23Y10")
-any(as.character(SamplePoints$Tile) %in% ExcludeTiles) # Make sure we don't discard something we need
-TileList = TileList[!TileList %in% ExcludeTiles]
-
-Tile = "X16Y06" # Test tile
+# Keep only the tiles that are in the reference data
+TileList = TileList[TileList %in% levels(SamplePoints$Tile)]
 
 # Generate a list of directories to read from.
 # This list is semi-static: we don't read in more files when they arrive. Else the lengths would differ.
@@ -30,10 +29,11 @@ if (!file.exists(DataDirFile))
     write.csv(DataDirsDates, DataDirFile, row.names=FALSE)
 } else {
     DataDirsDates = read.csv(DataDirFile, stringsAsFactors=FALSE)
+    DataDirsDates$date = as.Date(DataDirsDates$date)
     DataDirs = DataDirsDates$dir
 }
 
-BuildProbaVTileVRT = function(DataDirs, VI, Tile, Band=1)
+BuildProbaVTileVRT = function(DataDirs, VI, Tile, Band=1, OutputDir="../../userdata/master-classification/pixel-based/vrt/sr/")
 {
     TileFiles = list.files(DataDirs, pattern=glob2rx(paste0("PROBAV_S5_TOC_", Tile, "*", VI, ".tif")), full.names=TRUE)
     Dates = getProbaVinfo(TileFiles)$date
@@ -50,32 +50,48 @@ BuildProbaVTileVRT = function(DataDirs, VI, Tile, Band=1)
     return(Result)
 }
 
-SamplePoints = LoadGlobalTrainingData()
-PointsInTile = SamplePoints[SamplePoints$Tile == Tile,]
+# Extract time series data from the Proba-V tiles and mask them with the quality control layer.
+# Returns a matrix of extracted, masked values.
+# FilterQC values are based on GetProbaVQCMask(bluegood=TRUE, redgood=TRUE, nirgood=TRUE, swirgood=TRUE, ice=FALSE, cloud=FALSE, shadow=FALSE)
+ExtractPixelData = function(ExtractLocations, DataDirs, VI, Tile, Band, QCMatrix, FilterQC=c(240, 248), OutputDir="../data/pixel-based")
+{
+    OutputCSVFile = file.path(OutputDir, paste(Tile, VI, Band, ".csv", sep="-"))
+    if (file.exists(OutputCSVFile))
+        return(read.csv(OutputCSVFile))
+        
+    BandStack = BuildProbaVTileVRT(DataDirs, VI, Tile, Band)
+    BandValueMatrix = extract(BandStack, ExtractLocations)
+    BandValueMatrix[!QCMatrix %in% FilterQC] = NA
+    colnames(BandValueMatrix) = paste0(Band, "-", c(getZ(BandStack)))
+    write.csv(BandValueMatrix, OutputCSVFile, row.names=FALSE)
+    return(BandValueMatrix)
+}
 
-# Extract QC values
-QCStack = BuildProbaVTileVRT(DataDirs, "SM", Tile)
-any(duplicated(getZ(QCStack)))
-system.time(QCInTile <- extract(QCStack, PointsInTile))
-
-# Extract NDVI values
-TileStack = BuildProbaVTileVRT(DataDirs, "NDVI", Tile)
-all(getZ(QCStack) == getZ(TileStack))
-
-# Takes about 8 minutes per tile
-system.time(NDVIInTile <- extract(TileStack, PointsInTile))
-colnames(NDVIInTile) = paste0("NDVI-", c(getZ(TileStack)))
-
-# Mask with QC
-FilterQC = GetProbaVQCMask(bluegood=TRUE, redgood=TRUE, nirgood=TRUE, swirgood=TRUE,
-                           ice=FALSE, cloud=FALSE, shadow=FALSE)
-NDVIInTile[!QCInTile %in% FilterQC] = NA
-
-visdat::vis_miss(as.data.frame(NDVIInTile))
-write.csv(NDVIInTile, file.path("..", "data", "pixel-based", paste0(Tile, "-NDVI.csv")), row.names=FALSE)
-NDVIInTile = as.matrix(read.csv(file.path("..", "data", "pixel-based", paste0(Tile, "-NDVI.csv"))))
-colnames(NDVIInTile) = paste0("NDVI-", c(getZ(TileStack)))
+registerDoParallel(cores=4)
+foreach(Tile=iter(TileList), .inorder=FALSE, .multicombine=TRUE) %dopar%
+{
+    PointsInTile = SamplePoints[SamplePoints$Tile == Tile,]
+    if (nrow(PointsInTile) < 1)
+        print(paste("Skipping tile", Tile, "because there are no samples here"))
+    else
+    {
+        print(paste("Processing tile", Tile))
+        
+        # Extract QC values
+        QCStack = BuildProbaVTileVRT(DataDirs, "SM", Tile)
+        print(any(duplicated(getZ(QCStack))))
+        system.time(QCInTile <- extract(QCStack, PointsInTile))
+        
+        # Extract NDVI values
+        ExtractPixelData(PointsInTile, DataDirs, "NDVI", Tile, 1, QCInTile)
+        print("NDVI extraction complete, moving on to radiometry")
+        
+        # Radiometry: do it for 4 bands (Red, NIR, Blue, SWIR)
+        for (Band in 1:4)
+            ExtractPixelData(PointsInTile, DataDirs, "RADIOMETRY", Tile, Band, QCInTile)
+    }
+}
 
 # Could merge into one if needed
-cbind(PointsInTile, NDVIInTile)
+#cbind(PointsInTile, NDVIInTile)
 
