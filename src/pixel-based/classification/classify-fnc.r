eba9257984b153f51c1ccfa865fddc76b3a8ff64
Dainius <pastas4@gmail.com>
2018-08-23 18:09:25 +0200

    Start analysing fuzzy nearest centroid output

--- src/pixel-based/classification/classify-fnc.r
+++ src/pixel-based/classification/classify-fnc.r
@@ -1,6 +1,8 @@
 source("pixel-based/utils/load-sampling-data.r")
 source("pixel-based/utils/covariate-names.r")
+source("pixel-based/utils/subpixel-confusion-matrix.r") # Replace with package eventually
 source("utils/accuracy-statistics.r")
+source("pixel-based/utils/crossvalidation.r")
 
 Data.df = LoadTrainingAndCovariates()
 class(Data.df) = "data.frame"
@@ -11,4 +13,7 @@ FullFormula = paste("dominant_lc", paste0(GetAllPixelCovars(), collapse="+"), se
 
 library(GSIF)
 test = spfkm(formula(FullFormula), Data.df["dominant_lc"], Data.df[,GetAllPixelCovars()])
+
+AccuracyStatisticsPlots(test@mu, Data.df[, colnames(test@mu)]/100) # RMSE of 24%
+SCM(test@mu, as.matrix(Data.df[, colnames(test@mu)]/100), plot=TRUE, totals=TRUE, scale=TRUE) # 49% accuracy, kappa 0.39: pretty poor
 
